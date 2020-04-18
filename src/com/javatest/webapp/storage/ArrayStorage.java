9f6be453d3091edcc787500774d19f58dada8219
Bugaga159 <santechserp@gmail.com>
2020-04-18 20:58:05 +0300

    add Arrays invocation

--- src/com/javatest/webapp/storage/ArrayStorage.java
+++ src/com/javatest/webapp/storage/ArrayStorage.java
@@ -2,6 +2,8 @@ package com.javatest.webapp.storage;
 
 import com.javatest.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -10,9 +12,7 @@ public class ArrayStorage {
     protected int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
@@ -60,11 +60,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] res = new Resume[size()];
-        for (int i = 0; i < size(); i++) {
-            res[i] = storage[i];
-        }
-        return res;
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
